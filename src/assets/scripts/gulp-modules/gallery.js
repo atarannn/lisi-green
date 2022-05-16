d872e9612da8cd7d4d3ae6325f36774087d9eef2
atarannn <anastasiatarannn@icloud.com>
2022-05-16 23:15:34 +0300

    added changes for gallery slides

--- src/assets/scripts/gulp-modules/gallery.js
+++ src/assets/scripts/gulp-modules/gallery.js
@@ -1,27 +1,27 @@
-const parallaxAnimLogo = document.querySelectorAll('.gallery-column.middle-column');
-parallaxAnimLogo.forEach(section => {
-    gsap
-        .timeline({
-            paused: true,
-            scrollTrigger: {
-                trigger: section,
-                scrub: 1,
-                scroller: '.gallery-container',
-        },
-    })
-    .fromTo(
-            section,
-            {
-                y: 0,
-            },
-            {
-                y: -481,
-                ease: 'ease',
-            },
-        );
-    });
+// const parallaxAnimLogo = document.querySelectorAll('.gallery-column.middle-column');
+// parallaxAnimLogo.forEach(section => {
+//     gsap
+//         .timeline({
+//             paused: true,
+//             scrollTrigger: {
+//                 trigger: section,
+//                 scrub: 1,
+//                 scroller: '.gallery-container',
+//         },
+//     })
+//     .fromTo(
+//             section,
+//             {
+//                 y: 0,
+//             },
+//             {
+//                 y: 481,
+//                 ease: 'ease',
+//             },
+//         );
+//     });
 
-const parallaxAnimLogo2 = document.querySelectorAll('.gallery-column');
+const parallaxAnimLogo2 = document.querySelectorAll('.left-right-column');
 parallaxAnimLogo2.forEach(section => {
     gsap
         .timeline({
@@ -38,7 +38,7 @@ parallaxAnimLogo2.forEach(section => {
                 y: 0,
             },
             {
-                y: 481,
+                y: -500,
                 ease: 'ease',
             },
         );
