d232b3be7d9c01fc85c26099cd63b0ca6a88e992
yatsinoa <sashayatzino@gmail.com>
2024-03-27 16:12:31 +0200

    cookie js

--- src/assets/scripts/gulp-modules/header.js
+++ src/assets/scripts/gulp-modules/header.js
@@ -1,48 +1,61 @@
 if (document.documentElement.clientWidth > 575) {
-    document.onscroll = function () {
-        scrollFunction();
-    };
-    function scrollFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.querySelector('.header').classList.add('not-on-top');
-        } else {
-            document.querySelector('.header').classList.remove('not-on-top');
-        }
+  document.onscroll = function() {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.querySelector('.header').classList.add('not-on-top');
+    } else {
+      document.querySelector('.header').classList.remove('not-on-top');
     }
+  }
 }
 function menuOpen(menu) {
-    menu.classList.add('active');
-    const createAnimation = (links, delay = 0) => {
-        links.forEach((link, i) => {
-            gsap.from(link, {
-                delay: delay + i / 7,
-                opacity: 0,
-            });
-        });
-    };
-    const links = document.querySelectorAll('[data-animation]');
-    createAnimation(links, 0.3);
-    const tl = gsap.timeline({ paused: true });
-    tl.set(menu, { autoAlpha: 1 });
-    tl.fromTo(menu,
-        {x: 2000},
-        {x: 0, duration: 0.4}, '<');
-    tl.play();
-    document.querySelector('body').style.overflow = 'hidden';
+  menu.classList.add('active');
+  const createAnimation = (links, delay = 0) => {
+    links.forEach((link, i) => {
+      gsap.from(link, {
+        delay: delay + i / 7,
+        opacity: 0,
+      });
+    });
+  };
+  const links = document.querySelectorAll('[data-animation]');
+  createAnimation(links, 0.3);
+  const tl = gsap.timeline({ paused: true });
+  tl.set(menu, { autoAlpha: 1 });
+  tl.fromTo(menu, { x: 2000 }, { x: 0, duration: 0.4 }, '<');
+  tl.play();
+  document.querySelector('body').style.overflow = 'hidden';
 }
 function menuClose(menu) {
-    menu.classList.remove('active');
-    const tl = gsap.timeline({ paused: true });
-    tl.fromTo(menu,
-        { autoAlpha: 1, x: 0, },
-        { x: 2000, duration: 0.4, clearProps: 'all' }, '<');
-    tl.set(menu, { autoAlpha: 0 });
-    tl.play();
-    document.querySelector('body').style.overflow = 'auto';
+  menu.classList.remove('active');
+  const tl = gsap.timeline({ paused: true });
+  tl.fromTo(menu, { autoAlpha: 1, x: 0 }, { x: 2000, duration: 0.4, clearProps: 'all' }, '<');
+  tl.set(menu, { autoAlpha: 0 });
+  tl.play();
+  document.querySelector('body').style.overflow = 'auto';
 }
 function menuInit() {
-    const menu = document.querySelector('.menu-wrap');
-    document.querySelector('[data-open-menu]').addEventListener('click', () => menuOpen(menu));
-    document.querySelector('[data-close-menu]').addEventListener('click', () => menuClose(menu));
+  const menu = document.querySelector('.menu-wrap');
+  document.querySelector('[data-open-menu]').addEventListener('click', () => menuOpen(menu));
+  document.querySelector('[data-close-menu]').addEventListener('click', () => menuClose(menu));
 }
 window.addEventListener('DOMContentLoaded', menuInit);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var consentBanner = document.getElementById('cookies');
+  var acceptCookiesBtn = document.getElementById('accept-cookie');
+
+  acceptCookiesBtn.addEventListener('click', function() {
+    document.cookie = 'cookieConsent=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    sessionStorage.setItem('cookieConsent', 'true');
+    consentBanner.style.display = 'none';
+  });
+
+  if (sessionStorage.getItem('cookieConsent') === 'true') {
+    consentBanner.style.display = 'none';
+  } else {
+    consentBanner.style.display = 'block';
+  }
+});
